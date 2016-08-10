197e46d947644dc52ce04c8718e1e49a0cebc8d5
Fabrizio Fenoglio <fabri_feno@yahoo.it>
2016-08-10 15:59:47 +0100

    Splitting translations

--- index.js
+++ index.js
@@ -1,58 +1,93 @@
+var _ = require('lodash');
+
 /**
  * Created by ogi on 27.05.16.
  */
 function ReactIntlPlugin(options) {
+  this.options = options;
 }
 
 ReactIntlPlugin.prototype.apply = function (compiler) {
 
-    var messages = {};
+  var __this = this;
+  var messages = {};
 
-    compiler.plugin("compilation", function (compilation) {
-        // console.log("The compiler is starting a new compilation...");
+  compiler.plugin("compilation", function (compilation) {
+    // console.log("The compiler is starting a new compilation...");
 
-        compilation.plugin("normal-module-loader", function (context, module) {
-            // console.log("registering function: ", __dirname, "in loader context");
-            context["metadataReactIntlPlugin"] = function (metadata) {
-                // do something with metadata and module
-                // console.log("module:",module,"collecting metadata:", metadata);
-                messages[module.resource] = metadata["react-intl"].messages;
-            };
-        });
+    compilation.plugin("normal-module-loader", function (context, module) {
+      // console.log("registering function: ", __dirname, "in loader context");
+      context["metadataReactIntlPlugin"] = function (metadata) {
+        // do something with metadata and module
+        // console.log("module:",module,"collecting metadata:", metadata);
+        messages[module.resource] = metadata["react-intl"].messages;
+      };
+    });
+  });
+
+  compiler.plugin('emit', function (compilation, callback) {
+    // console.log("emitting messages");
+
+    // check for duplicates and flatten
+    var jsonMessages = [];
+    var idIndex = {};
+    Object.keys(messages).map(function (e) {
+      messages[e].map(function (m) {
+        if (!idIndex[m.id]) {
+          idIndex[m.id] = e;
+          jsonMessages.push(m);
+        } else {
+          compilation.errors.push("ReactIntlPlugin -> duplicate id: '" + m.id + "'.Found in '" + idIndex[m.id] + "' and '" + e + "'.");
+        }
+      })
     });
 
-    compiler.plugin('emit', function (compilation, callback) {
-        // console.log("emitting messages");
+    var jsonString = JSON.stringify(jsonMessages, undefined, 2);
+    // console.log("jsonString:",jsonString);
 
-        // check for duplicates and flatten
-        var jsonMessages = [];
-        var idIndex = {};
-        Object.keys(messages).map(function (e) {
-            messages[e].map(function (m) {
-                if (!idIndex[m.id]) {
-                    idIndex[m.id] = e;
-                    jsonMessages.push(m);
-                } else {
-                    compilation.errors.push("ReactIntlPlugin -> duplicate id: '" + m.id + "'.Found in '" + idIndex[m.id] + "' and '" + e + "'.");
-                }
-            })
-        });
+    var fileName = __this.options.defaultLang + '.json' || 'en.json';
+    var supportedLangs = __this.options.supportedLangs;
 
-        var jsonString = JSON.stringify(jsonMessages, undefined, 2);
-        // console.log("jsonString:",jsonString);
+    if (Array.isArray(supportedLangs)) {
+      supportedLangs.forEach(function (langObj) {
+        var currentLangFile = require(langObj.file);
+        var langName = langObj.lang;
 
-        // Insert this list into the Webpack build as a new file asset:
-        compilation.assets['reactIntlMessages.json'] = {
-            source: function () {
-                return jsonString;
-            },
-            size: function () {
-                return jsonString.length;
-            }
+        var messagesIntoLang = jsonMessages.reduce(function (obj, message) {
+          var transDone = _.find(currentLangFile, { id: message.id });
+
+          if (transDone) {
+            return transDone;
+          } else {
+            return { id: message.id, defaultMessage: '' };
+          }
+        }, []);
+
+        var messagesLangJson = JSON.stringify(messagesIntoLang, undefined, 2);
+
+        compilation.assets[langName + '.json'] = {
+          source: function () {
+            return messagesLangJson;
+          },
+          size: function () {
+            return messagesLangJson.length;
+          }
         };
+      });
+    }
 
-        callback();
-    });
+    // Insert this list into the Webpack build as a new file asset:
+    compilation.assets[fileName] = {
+      source: function () {
+        return jsonString;
+      },
+      size: function () {
+        return jsonString.length;
+      }
+    };
+
+    callback();
+  });
 };
 
 module.exports = ReactIntlPlugin;
